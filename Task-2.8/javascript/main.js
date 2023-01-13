65874aec3e87890a83516a76bb0b79c25a695737
ArturRubets <artur.rubets@gmail.com>
2023-01-13 12:01:38 +0200

    Add banner and catalog

--- Task-2.8/javascript/main.js
+++ Task-2.8/javascript/main.js
@@ -21,12 +21,26 @@ const handleSearch = () => {
 
 inputSearch.addEventListener('input', handleSearch);
 
-const flickity = new Flickity('.carousel', {
-  groupCells: true,
-  cellAlign: 'left',
+const carouselBanners = new Flickity('.carousel-banners', {
+  wrapAround: true,
+  autoPlay: 5000,
+  fade: true,
+  arrowShape: {
+    x0: 10,
+    x1: 40,
+    y1: 50,
+    x2: 45,
+    y2: 45,
+    x3: 20,
+  },
+});
+
+const carouselProducts = new Flickity('.carousel-products', {
+  groupCells: 2,
+  cellAlign: 'right',
   contain: true,
   pageDots: false,
-  adaptiveHeight: true,
+  freeScroll: true,
   arrowShape: {
     x0: 0,
     x1: 60,
